14f06e370dbb3c9dfa04e01bd74931f264211433
ZhengLin-Li <63448884+ZhengLin-Li@users.noreply.github.com>
2021-05-13 11:06:02 +0800

    Add files via upload

---
+++ saolei/cmake-build-debug/CMakeFiles/saolei.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/code is here/saolei/Controller.cpp" "D:/code is here/saolei/cmake-build-debug/CMakeFiles/saolei.dir/Controller.cpp.obj"
+  "D:/code is here/saolei/GameEngine.cpp" "D:/code is here/saolei/cmake-build-debug/CMakeFiles/saolei.dir/GameEngine.cpp.obj"
+  "D:/code is here/saolei/GameEnginePro.cpp" "D:/code is here/saolei/cmake-build-debug/CMakeFiles/saolei.dir/GameEnginePro.cpp.obj"
+  "D:/code is here/saolei/GameMenu.cpp" "D:/code is here/saolei/cmake-build-debug/CMakeFiles/saolei.dir/GameMenu.cpp.obj"
+  "D:/code is here/saolei/Graphic.cpp" "D:/code is here/saolei/cmake-build-debug/CMakeFiles/saolei.dir/Graphic.cpp.obj"
+  "D:/code is here/saolei/Main.cpp" "D:/code is here/saolei/cmake-build-debug/CMakeFiles/saolei.dir/Main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ saolei/cmake-build-debug/CMakeFiles/saolei.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/saolei.dir/Controller.cpp.obj"
+  "CMakeFiles/saolei.dir/GameEngine.cpp.obj"
+  "CMakeFiles/saolei.dir/GameEnginePro.cpp.obj"
+  "CMakeFiles/saolei.dir/GameMenu.cpp.obj"
+  "CMakeFiles/saolei.dir/Graphic.cpp.obj"
+  "CMakeFiles/saolei.dir/Main.cpp.obj"
+  "libsaolei.dll.a"
+  "saolei.exe"
+  "saolei.exe.manifest"
+  "saolei.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/saolei.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
