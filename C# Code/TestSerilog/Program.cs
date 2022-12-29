c51ddac5707830ed9fbdf7b41701e133fb1842ac
v-zhenglinli <v-zhenglinli@microsoft.com>
2022-12-29 13:42:14 +0800

    add test serilog

---
+++ C# Code/TestSerilog/Program.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File("log.txt",
+                rollingInterval: RollingInterval.Day,
+                rollOnFileSizeLimit: true)
+            .CreateLogger();
+            
+        Log.Information("Hello, Serilog! {}", "test if can seen");
+        Log.Information("Hello, Serilog! {0}", "test if can seen");
+        
+        Log.CloseAndFlush();
+    }
+}
